75cd53bf78437a15b6491c7bfb2e3c72b0721f8f
R-umaria <145788470+R-umaria@users.noreply.github.com>
2024-12-05 11:45:46 -0500

    chnaged parental senttings ui

--- lib/pages/parental_settings.dart
+++ lib/pages/parental_settings.dart
@@ -38,8 +38,10 @@ class _ParentalSettingsPageState extends State<ParentalSettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromRGBO(248, 244, 234, 1), //match to the main screen bg colour
       appBar: AppBar(
-        title: Text('Parental Settings'),
+        title: Text('Parental Settings', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
+        backgroundColor: const Color.fromRGBO(86, 170, 200, 1),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -55,28 +57,45 @@ class _ParentalSettingsPageState extends State<ParentalSettingsPage> {
               },
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Speed Limit: ${speedLimit.toInt()} km/h'),
-                Slider(
-                  value: speedLimit,
-                  min: 20,
-                  max: 150,
-                  divisions: 13,
-                  label: speedLimit.toInt().toString(),
-                  onChanged: (value) {
-                    setState(() {
-                      speedLimit = value;
-                    });
-                  },
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: saveSettings,
-              child: Text('Save Settings'),
-            ),
+  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  children: [
+    Flexible(
+      child: Text(
+        'Speed Limit: ${speedLimit.toInt()} km/h',
+        overflow: TextOverflow.ellipsis, // Prevent long text from overflowing
+      ),
+    ),
+    Expanded(
+      child: Slider(
+        value: speedLimit,
+        min: 20,
+        max: 150,
+        divisions: 13,
+        label: speedLimit.toInt().toString(),
+        activeColor: Color.fromRGBO(86, 170, 200, 1), // Color of the filled part of the slider
+        inactiveColor: Colors.grey, // Color of the unfilled part of the slider
+        onChanged: (value) {
+          setState(() {
+            speedLimit = value;
+          });
+        },
+      ),
+    ),
+  ],
+),
+SizedBox(height: 20),
+ElevatedButton(
+  onPressed: saveSettings,
+  style: ElevatedButton.styleFrom(
+        backgroundColor: const Color.fromRGBO(86, 170, 200, 1), // Button background color
+    foregroundColor: Colors.white, // Button text color
+    padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(8.0), // Rounded button corners
+    ),
+  ),
+  child: Text('Save Settings'),
+),
           ],
         ),
       ),
